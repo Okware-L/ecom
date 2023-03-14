b029e6b46e2f4515fbf7e5ed496c5c048749b8ed
Okware-L <okware@DESKTOP-996KE1O>
2023-03-14 23:54:09 +0300

    ecom

---
+++ src/scenes/home/MainCarousel.jsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const MainCarousel = () => {
+
+  return (
+    <Box>
+      <Typography>Text</Typography>
+    </Box>
+  );
+};
+
+export default MainCarousel;
